8352ca870a3abff271936367b8c4ed23ac7b358e
Jeff Ripke <jripke74@gmail.com>
2025-06-27 10:46:22 -0700

    add js

--- script.js
+++ script.js
@@ -28,6 +28,18 @@ function sortByYear(book1, book2) {
   }
 }
 
-const filteredBooks = books.filter((book) => book.releaseYear > 1950);
+// function sortByYear(book1, book2) {
+//   if (book1.releaseYear < book2.relaeseYear) {
+//     return -1;
+//   } else if (book1.releaseYear > book2.releaseYear) {
+//     return 1;
+//   } else {
+//     return 0
+//   }
+// }
 
+// const filteredBooks = books.filter(book => book.releaseYear > 1950).sort();
+
+const filteredBooks = books.filter(book => book.releaseYear > 2021);
 filteredBooks.sort(sortByYear);
+console.log(filteredBooks);
